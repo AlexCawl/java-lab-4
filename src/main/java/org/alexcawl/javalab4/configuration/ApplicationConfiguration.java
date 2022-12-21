58485d50a4d7552ecd82d788f99ec9eb37e54160
AlexCawl <babushkinsusu@gmail.com>
2022-12-21 13:45:35 +0500

    make PingContoller

--- src/main/java/org/alexcawl/javalab4/configuration/ApplicationConfiguration.java
+++ src/main/java/org/alexcawl/javalab4/configuration/ApplicationConfiguration.java
@@ -5,9 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableScheduling
+@EnableSwagger2
 public class ApplicationConfiguration {
     @Bean
     public TaskScheduler taskScheduler() {
--- src/main/java/org/alexcawl/javalab4/controller/BaseController.java
+++ src/main/java/org/alexcawl/javalab4/controller/BaseController.java
@@ -1,4 +1,14 @@
 package org.alexcawl.javalab4.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/v1", name = "Base Controller")
 public class BaseController {
+    @RequestMapping(path = "/ping", name = "ping", method = RequestMethod.GET)
+    public String ping() {
+        return "pong!";
+    }
 }
